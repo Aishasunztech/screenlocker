d601ec3c98d30825c79cd0997594c26a874c4922
Muhammad Mehran <imuhammadmehran@gmail.com>
2019-05-01 17:27:42 +0500

    Titan Locker version 4.66

--- app/src/main/java/com/screenlocker/secure/utils/AppConstants.java
+++ app/src/main/java/com/screenlocker/secure/utils/AppConstants.java
@@ -197,11 +197,11 @@ public class AppConstants {
 
 
     public static int attempt_5 = 1;
-    public static int attempt_6 = 5;
-    public static int attempt_7 = 10;
-    public static int attempt_8 = 15;
-    public static int attempt_9 = 30;
-    public static int attempt_10 = 30;
+    public static int attempt_6 = 1;
+    public static int attempt_7 = 3;
+    public static int attempt_8 = 5;
+    public static int attempt_9 = 5;
+    public static int attempt_10 = 5;
 
 
     // acure xlear
