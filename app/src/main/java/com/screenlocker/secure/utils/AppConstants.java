0ad1a7df255a9376519295040dfe9ae3af02b384
Muhammad Mehran <imuhammadmehran@gmail.com>
2019-06-10 20:00:06 +0500

    Screen Locker for neutral launcher (4.79)

--- app/src/main/java/com/screenlocker/secure/utils/AppConstants.java
+++ app/src/main/java/com/screenlocker/secure/utils/AppConstants.java
@@ -76,8 +76,9 @@ public class AppConstants {
 
 
     //servers links
-//    public static final String URL_1 = "http://api.lockmesh.com";//live
-        public static final String URL_1 = "http://192.168.0.118:3000";//local
+//    public static final String URL_1 = "http://api.lockmesh.com";//live SL
+    public static final String URL_1 = "http://165.22.82.254:3000";//live TL
+//        public static final String URL_1 = "http://192.168.0.118:3000";//local
     public static final String URL_2 = "http://securenet.guru";
 
     //super admin domain
@@ -243,22 +244,22 @@ public class AppConstants {
     /*
      * Screen Locker
      * */
-//    public static int attempt_5 = 1;
-//    public static int attempt_6 = 5;
-//    public static int attempt_7 = 10;
-//    public static int attempt_8 = 15;
-//    public static int attempt_9 = 30;
-//    public static int attempt_10 = 30;
+    public static int attempt_5 = 1;
+    public static int attempt_6 = 5;
+    public static int attempt_7 = 10;
+    public static int attempt_8 = 15;
+    public static int attempt_9 = 30;
+    public static int attempt_10 = 30;
 
     /*
      *Titan Locker
      * */
-    public static int attempt_5 = 1;
-    public static int attempt_6 = 1;
-    public static int attempt_7 = 3;
-    public static int attempt_8 = 5;
-    public static int attempt_9 = 5;
-    public static int attempt_10 = 5;
+//    public static int attempt_5 = 1;
+//    public static int attempt_6 = 1;
+//    public static int attempt_7 = 3;
+//    public static int attempt_8 = 5;
+//    public static int attempt_9 = 5;
+//    public static int attempt_10 = 5;
 
 
     public static final String IMEI_CHANGED = "imeiChanged";
