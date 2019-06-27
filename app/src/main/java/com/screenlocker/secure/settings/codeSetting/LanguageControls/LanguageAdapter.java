d60bc9d3d350020a1fa888ee93f4a6081d1893e1
Abdul Razzaq <mehran@sunztech.com>
2019-06-27 11:06:27 +0500

    vietnam translation done

--- app/src/main/java/com/screenlocker/secure/settings/codeSetting/LanguageControls/LanguageAdapter.java
+++ app/src/main/java/com/screenlocker/secure/settings/codeSetting/LanguageControls/LanguageAdapter.java
@@ -1,14 +1,19 @@
 package com.screenlocker.secure.settings.codeSetting.LanguageControls;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -54,31 +59,88 @@ public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.MyView
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView tv_lang;
+        ImageView img_flag;
+        CheckBox checkBox;
+        View view;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             tv_lang = itemView.findViewById(R.id.langName);
+            img_flag = itemView.findViewById(R.id.img_flag);
+            checkBox = itemView.findViewById(R.id.language_check);
+            view = itemView.findViewById(R.id.language_divider);
             itemView.setOnClickListener(this);
         }
 
         public void setData(LanguageModel languageModel) {
+
+            LanguageModel model = langList.get(getAdapterPosition());
             tv_lang.setText(languageModel.getLanguage_name());
+
+            if(model.getLanguage_key().equals("en"))
+            {
+                img_flag.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_flag_of_the_united_states));
+            }
+            else if(model.getLanguage_key().equals("fr"))
+            {
+                img_flag.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_flag_of_france));
+            }else if(model.getLanguage_key().equals("vi"))
+            {
+                img_flag.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_flag_of_vietnam));
+            }
+
+            String key = PrefUtils.getStringPref(context,AppConstants.LANGUAGE_PREF);
+           if(model.getLanguage_key().equals(key))
+            {
+                checkBox.setChecked(true);
+            }
+           checkBox.setClickable(false);
+           if(getAdapterPosition() == langList.size()-1){
+               view.setVisibility(View.GONE);
+           }else{
+               view.setVisibility(View.VISIBLE);
+           }
+
         }
 
         @Override
         public void onClick(View v) {
+            showLanguageDialog();
 
+        }
+
+        private void showLanguageDialog() {
+            AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+            alertDialog.setTitle(context.getResources().getString(R.string.change_language_dialog_title));
+            alertDialog.setMessage(context.getResources().getString(R.string.change_language_dialog_message));
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, context.getResources().getString(R.string.ok_text), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    changeLanguage();
+                    dialog.dismiss();
+                }
+            });
+            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, context.getResources().getString(R.string.cancel_capital), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            alertDialog.setCancelable(false);
+
+            alertDialog.show();
+        }
+
+        private void changeLanguage() {
             LanguageModel languageModel = langList.get(getAdapterPosition());
             CommonUtils.setAppLocale(languageModel.getLanguage_key(), context);
             PrefUtils.saveStringPref(context, AppConstants.LANGUAGE_PREF, languageModel.getLanguage_key());
             listener.recreatActivity();
-            Toast.makeText(context, context.getResources().getString(R.string.language_is_changed), Toast.LENGTH_SHORT).show();
 
             Intent intent = new Intent(BROADCAST_APPS_ACTION);
             intent.putExtra(KEY_DATABASE_CHANGE, "apps");
             LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-
         }
     }
 
